bf3ba21863e415788a06c7e652d43f3ec02cfb6c
Mahmoud Taleb <32706209+mahmoudTaleb@users.noreply.github.com>
2018-04-12 13:19:21 +0200

    Add billing class

--- src/main/java/com/capgemini/Model/Drinks.java
+++ src/main/java/com/capgemini/Model/Drinks.java
@@ -1,7 +1,54 @@
 package com.capgemini.Model;
 
 public class Drinks {
-    private double price;
-    private int itemNumber;
-    private String name;
+    private String itemName;
+    private double itemPrice;
+    private boolean available;
+
+    public Drinks(String itemName, double itemPrice, boolean available){
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.available = available;
+
+    }
+
+    public void drinkNameChange(String newName){
+        this.itemName = newName;
+    }
+
+
+    public void increasePrice(int addition){
+        itemPrice = itemPrice + addition;
+
+
+    }
+
+    public void decreasePrice(int discount){
+        itemPrice = itemPrice - discount;
+
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
